def20a4b2c324a7ca3ae5aa18ce2192fa18536a2
Artem <Artemvyhovanec@gmail.com>
2022-11-10 12:38:34 +0200

    Add CRUD for topic

--- news/urls.py
+++ news/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from news.views import index
+from news.views import *
 
 urlpatterns = [
-    path('', index)
+    path('', index, name="index"),
+    path('topics/', TopicListView.as_view(), name="topic-list"),
+    path("topic/create/", TopicCreateView.as_view(), name="topic-create"),
+    path("topic/update/<int:pk>/", TopicUpdateView.as_view(), name="topic-update"),
+    path("topic/delete/<int:pk>/", TopicDeleteView.as_view(), name="topic-delete")
 ]
 
 app_name = "news"
--- news/views.py
+++ news/views.py
@@ -1,6 +1,31 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+
+from news.models import Topic
 
 
 def index(request):
     return render(request, "news/index.html")
+
+
+class TopicListView(generic.ListView):
+    model = Topic
+
+
+class TopicCreateView(generic.CreateView):
+    model = Topic
+    fields = "__all__"
+    success_url = reverse_lazy("news:topic-list")
+
+
+class TopicUpdateView(generic.UpdateView):
+    model = Topic
+    fields = "__all__"
+    success_url = reverse_lazy("news:topic-list")
+
+
+class TopicDeleteView(generic.DeleteView):
+    model = Topic
+    success_url = reverse_lazy("news:topic-list")
