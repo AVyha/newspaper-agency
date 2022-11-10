aa1584efd1044bff726a929ddaab4a5498719da8
Artem <Artemvyhovanec@gmail.com>
2022-11-10 13:30:20 +0200

    Add layout for redactors CRUD

--- news/urls.py
+++ news/urls.py
@@ -7,7 +7,12 @@ urlpatterns = [
     path('topics/', TopicListView.as_view(), name="topic-list"),
     path("topic/create/", TopicCreateView.as_view(), name="topic-create"),
     path("topic/update/<int:pk>/", TopicUpdateView.as_view(), name="topic-update"),
-    path("topic/delete/<int:pk>/", TopicDeleteView.as_view(), name="topic-delete")
+    path("topic/delete/<int:pk>/", TopicDeleteView.as_view(), name="topic-delete"),
+    path("redactors/", RedactorListView.as_view(), name="redactor-list"),
+    path("redactor/create/", RedactorCreateView.as_view(), name="redactor-create"),
+    path("redactor/update/<int:pk>/", RedactorUpdateView.as_view(), name="redactor-update"),
+    path("redactor/delete/<int:pk>/", RedactorDeleteView.as_view(), name="redactor-delete"),
+    path("redactor/info/<int:pk>/", RedactorDetailView.as_view(), name="redactor-detail")
 ]
 
 app_name = "news"
--- news/views.py
+++ news/views.py
@@ -3,13 +3,14 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
-from news.models import Topic
+from news.models import Topic, Redactor
 
 
 def index(request):
     return render(request, "news/index.html")
 
 
+# Topic CRUD
 class TopicListView(generic.ListView):
     model = Topic
 
@@ -29,3 +30,29 @@ class TopicUpdateView(generic.UpdateView):
 class TopicDeleteView(generic.DeleteView):
     model = Topic
     success_url = reverse_lazy("news:topic-list")
+
+
+# Redactor CRUD
+class RedactorListView(generic.ListView):
+    model = Redactor
+
+
+class RedactorCreateView(generic.CreateView):
+    model = Redactor
+    success_url = reverse_lazy("news:index")
+    fields = "__all__"
+
+
+class RedactorUpdateView(generic.UpdateView):
+    model = Redactor
+    success_url = reverse_lazy("news:redactor-list")
+    fields = "__all__"
+
+
+class RedactorDeleteView(generic.DeleteView):
+    model = Redactor
+    success_url = reverse_lazy("news:redactor-list")
+
+
+class RedactorDetailView(generic.DetailView):
+    model = Redactor
